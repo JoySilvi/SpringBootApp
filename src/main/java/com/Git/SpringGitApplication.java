6e852eb7feafc9b7d7323a4475d0e15469ceada4
JoySilvi <ejoysilvi@gmail.com>
2025-01-20 16:25:48 +0530

    changes done in base class

--- src/main/java/com/Git/SpringGitApplication.java
+++ src/main/java/com/Git/SpringGitApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringGitApplication {
+private Map<String,Object>=new HashMap<String,Object>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringGitApplication.class, args);
