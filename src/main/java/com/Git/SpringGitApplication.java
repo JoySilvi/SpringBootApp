92be2890fb3a17d377252283a15d95616ec0cfe6
JoySilvi <ejoysilvi@gmail.com>
2025-01-20 16:31:19 +0530

    feature-4 in base class changes are done

--- src/main/java/com/Git/SpringGitApplication.java
+++ src/main/java/com/Git/SpringGitApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringGitApplication {
+private List<Long> l=new ArrayList<Long>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringGitApplication.class, args);
